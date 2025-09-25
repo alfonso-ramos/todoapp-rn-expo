49d53d66bc5fe15c51c6ec6bf572ac6fd0959e1b
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2025-09-24 22:20:48 -0700

    [ADD] customhook para darkmode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,18 @@
-import { Text, View, StyleSheet } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const {toggleDarkMode} = useTheme()
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
         Edit app/index.tsx to edit this screen.123
       </Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>
+          Toggle the mode
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
