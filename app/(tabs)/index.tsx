fce2767b47599c93bcda1ff9a3ef9269674a8d51
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2025-09-29 23:01:37 -0700

    [ADD] estilos de fonod y safeareaView para no exceder borders

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,25 @@
+import { createHomeStyles } from "@/assets/images/styles/home.styles";
 import useTheme from "@/hooks/useTheme";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme()
+  const {toggleDarkMode, colors} = useTheme()
+
+  const homeStyles = createHomeStyles(colors)
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>
-        Edit app/index.tsx to edit this screen.123
-      </Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>
-          Toggle the mode
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+
+      <StatusBar barStyle={colors.statusBarStyle}/>
+      <SafeAreaView style={homeStyles.safeArea}>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>
+            Toggle the mode
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-      fontSize: 20,
-  },
-});
